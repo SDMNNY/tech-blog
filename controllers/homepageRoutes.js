71d78dcabd6c1aefbce41a364bc0274d1f0c9218
sdmnny <sdmnny@gmail.com>
2022-07-07 19:11:01 -0400

    api index file finalized

--- controllers/homepageRoutes.js
+++ controllers/homepageRoutes.js
@@ -51,3 +51,22 @@ router.get("/blogpost/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get("/login", (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+
+    res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect("/");
+        return;
+    }
+    res.render("signup");
+});
+
+module.exports = router; 
