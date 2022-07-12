53ff98f95d1420a7bfdfd9e1ee4b699622853c31
sdmnny <sdmnny@gmail.com>
2022-07-12 18:26:47 -0400

    finished blogpost routes file

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -34,3 +34,22 @@ router.put("/:id", withAuth, async (req, res) => {
     }
 });
 
+router.delete("/:id", withAuth, async (req,res) => { 
+    try { 
+        const deleteBlogData = await Blogpost.destroy({
+            where: { id: req.params.id },
+        });
+        
+        if(!deleteBlogData) { 
+            res 
+            .status(404)
+            .json({ message: "Could not find a blog post with this id" });
+        } else {
+            res.status(200).json(deleteBlogData);
+        }
+    } catch (err) { 
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router; 
