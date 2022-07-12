48c6912dddfee92a48afad3d80bc9f7355fea6fc
sdmnny <sdmnny@gmail.com>
2022-07-12 18:50:13 -0400

    login routes complete

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const { User } = router("../../models");
+
+router.post("/", async (req, res) => {
+    try { 
+        const userData = await User.create({ 
+            name: req.body.name, 
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try { 
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect email or password" });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if(!validPassword) { 
+            res.status(400).json({ message: "Incorrect email or password" });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.loggedIn = true; 
+
+            res.json({ user: userData, message: "Logged in sucessfully" });
+        });
+    } catch (err) { 
+        res.status(400).json(err);
+    }
+});
