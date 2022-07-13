ef46350e127488174e11e2ef5aec21e52b343558
sdmnny <sdmnny@gmail.com>
2022-07-12 21:44:59 -0400

    deleteblogpost.js complete

---
+++ public/js/deleteBlogPost.js
@@ -0,0 +1,23 @@
+const { Json } = require("sequelize/types/utils");
+
+const deleteBlogPost = async (event) => { 
+    event.preventDefault(); 
+
+    const id = document.querySelector("#blogpost-id").value;
+
+    const response = await fetch(`/api/blogposts/${id}`, {
+        method: "DELETE",
+        body: JSON.stringify({
+            id,
+        }),
+        headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+        document.location.replace("/dashboard");
+    } else {
+        alert("Failed to delete");
+    }
+};
+
+document.querySelector(".delBtn").addEventListener("click", deleteBlogPost);
